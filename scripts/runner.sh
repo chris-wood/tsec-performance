3d5b64b49ec83fa5540d1ca24cd1dc102c966d4a
Christopher Wood <christopherwood07@gmail.com>
2016-09-12 08:22:46 -0700

    New runner script for the TSec performance plots

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -4,11 +4,16 @@ PROGRAM=$1
 URI_FILE=$2
 PREFIX_LENGTH=$3
 
-OUTFILE=${URI_FILE}.out
-touch ${OUTFILE}
+OUTFILE_SHA256=${URI_FILE}_SHA256.out
+OUTFILE_Argon2=${URI_FILE}_Argon2.out
+touch ${OUTFILE_SHA256}
+touch ${OUTFILE_Argon2}
 
 for i in `seq 1 ${PREFIX_LENGTH}`;
 do
     echo ${PROGRAM} ${URI_FILE} ${i}
-    ${PROGRAM} ${URI_FILE} ${i} >> ${OUTFILE}
+    ${PROGRAM} ${URI_FILE} ${i} 0 >> ${OUTFILE_SHA256}
+
+    # default Argon2 parameters
+    ${PROGRAM} ${URI_FILE} ${i} 1 3 12 >> ${OUTFILE_Argon2}
 done
