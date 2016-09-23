4b89102cf97d2ea8fa368db127136e4785ddc168
Christopher Wood <christopherwood07@gmail.com>
2016-09-22 17:31:06 -0700

    More updates

--- scripts/profiler.py
+++ scripts/profiler.py
@@ -28,13 +28,16 @@ class Argon2Params(object):
 
         return params
 
-    def successors(self):
+    def successors(self, bound = None):
         params = []
-        params.append(Argon2Params(self.t + 1, self.m, self.d))
-        params.append(Argon2Params(self.t, self.m + 1, self.d))
-        params.append(Argon2Params(self.t, self.m, self.d + 1))
+        if bound == None or self.t < bound.t:
+            params.append(Argon2Params(self.t + 1, self.m, self.d))
+        if bound == None or self.m < bound.m:
+            params.append(Argon2Params(self.t, self.m + 1, self.d))
+        if bound == None or self.d < bound.d:
+            params.append(Argon2Params(self.t, self.m, self.d + 1))
         return params
-    
+
     def process(self, prog):
         process = subprocess.Popen([prog, "ARGON2", str(self.t), str(self.m), str(self.d)], stdout=subprocess.PIPE)
         pout, err = process.communicate()
@@ -82,84 +85,27 @@ class ScryptParams(object):
 
         return params
 
-### https://en.wikipedia.org/wiki/Branch_and_bound
-def optimize_bnb(prog, initialParams, target):
-    pass
-
-### https://en.wikipedia.org/wiki/Hill_climbing
-def optimize_hill(prog, initialParams, target):
-    current = initialParams
-    currentTime = -1
-    seen = set()
-    nextSet = []
-    while True:
-
-        L = current.neighbors()
-        for p in nextSet:
-            L.append(p)
-
-        random.shuffle(L)
-        if len(L) > 100:
-            L = L[0:100]
-        nextSet = []
-
-        nextEval = -1
-        nextNode = None
-
-        for param in L:
-            if str(param) not in seen: # don't repeat old params
-                seen.add(str(param))
-                thetime = param.process(prog)
-                for p in param.neighbors():
-                    nextSet.append(p)
-
-                if thetime > nextEval and thetime < target:
-                    nextNode = param
-                    nextEval = thetime
-
-        thetime = current.process(prog)
-        if thetime > nextEval and thetime < target:
-            return current, currentTime
-
-        # [currentTime ... nextEval ... target]
-        if nextEval > currentTime and nextEval < target:
-            current = nextNode
-            currentTime = nextEval
-        elif thetime > currentTime and thetime < target: # [nextEval ... currentTime ... target]
-            current = current
-            currentTime = thetime
-
-        print current, currentTime, len(L)
-    
-    return current, currentTime
-
-def optimize_dfs(prog, initialParams, target):
+def profile(prog, initialParams, boundParams):
+    ''' Simple DFS traversal of the parameter space...
+    '''
     queue = [initialParams]
-    visited = set()
     results = {}
+    visited = set()
     while len(queue) > 0:
-        current = queue.pop(0)
-        if str(current) not in visited:
-            visited.add(str(current))
+        curr = queue.pop(0)
+        if str(curr) not in visited:
+            visited.add(str(curr))
+            print >> sys.stderr, "Processing %s" % repr(curr)
+            delta = curr.process(prog)
+            results[str(curr)] = delta
 
-            thetime = current.process(prog)
-
-            #print current, thetime
-            if thetime < P:
-                for nextParams in current.successors():
-                    queue.append(nextParams)
-            else:
-                print "at end", current, thetime
-                results[current] = thetime
+            for nextParams in curr.successors(boundParams):
+                queue.append(nextParams)
     return results
+    
 
 prog = sys.argv[1]
-P = float(sys.argv[2])
-initialParams = Argon2Params()
-params, thetime = optimize_hill(prog, initialParams, P)
-print params, thetime
-
 initialParams = Argon2Params(1, 1, 1)
-results = optimize_dfs(prog, initialParams, P)
+boundParams = Argon2Params(10, 10, 2)
+results = profile(prog, initialParams, boundParams)
 print results
-
