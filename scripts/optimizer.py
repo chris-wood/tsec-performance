c487d9aedd45c3d35d4e1c53d27a16285645b0d2
Christopher Wood <christopherwood07@gmail.com>
2016-09-23 11:03:38 -0700

    Updated DFS optimization code to truncate early when the solution starts to converge

--- scripts/optimizer.py
+++ scripts/optimizer.py
@@ -32,39 +32,46 @@ class Argon2Params(object):
         params = []
         params.append(Argon2Params(self.t + 1, self.m, self.d))
         params.append(Argon2Params(self.t, self.m + 1, self.d))
-        params.append(Argon2Params(self.t, self.m, self.d + 1))
+        #params.append(Argon2Params(self.t, self.m, self.d + 1))
         return params
     
-    def process(self, prog):
-        process = subprocess.Popen([prog, "ARGON2", str(self.t), str(self.m), str(self.d)], stdout=subprocess.PIPE)
-        pout, err = process.communicate()
-        return float(pout) / 1000.0 # ns to us
+    def process(self, prog, N = 5):
+        total = 0.0
+        for i in range(N):
+            process = subprocess.Popen([prog, "ARGON2", str(self.t), str(self.m), str(self.d)], stdout=subprocess.PIPE)
+            pout, err = process.communicate()
+            total += (float(pout) / 1000.0)
+        return total / float(N)
 
     def __repr__(self):
         return "(%d, %d, %d)" % (self.t, self.m, self.d)
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return self.t == other.t and self.m == other.m and self.d == other.da
+            return self.t == other.t and self.m == other.m and self.d == other.d
         else:
             return False
 
     def __cmp__(self, other):
         if isinstance(other, self.__class__):
-            return self.t == other.t and self.m == other.m and self.d == other.da
+            return self.t == other.t and self.m == other.m and self.d == other.d
         else:
             return False
 
-def scrypt(prog, params):
-    process = subprocess.Popen([prog, "scrypt", str(params.N), str(params.r), str(params.p)], stdout=subprocess.PIPE)
-    pout, err = process.communicate()
-    return float(pout) / 1000.0 # ns to us
 
 class ScryptParams(object):
     def __init__(self, N = 1, r = 1, p = 1):
         self.N = N
         self.r = r
         self.p = p
+
+    def process(self, prog, N = 5):
+        total = 0.0
+        for i in range(N):
+            process = subprocess.Popen([prog, "scrypt", str(self.N), str(self.r), str(self.p)], stdout=subprocess.PIPE)
+            pout, err = process.communicate()
+            total += (float(pout) / 1000.0)
+        return total / N
 
     def neighbors(self):
         params = []
@@ -81,6 +88,27 @@ class ScryptParams(object):
         params.append(ScryptParams(self.N, self.r, self.p + 1))
 
         return params
+
+    def successors(self):
+        params = []
+        params.append(ScryptParams(self.N + 1, self.r, self.p))
+        params.append(ScryptParams(self.N, self.r + 1, self.p))
+        return params
+
+    def __repr__(self):
+        return "(%d, %d, %d)" % (self.N, self.r, self.p)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.N == other.N and self.r == other.r and self.p == other.p
+        else:
+            return False
+
+    def __cmp__(self, other):
+        if isinstance(other, self.__class__):
+            return self.N == other.N and self.r == other.r and self.p == other.p
+        else:
+            return False
 
 ### https://en.wikipedia.org/wiki/Branch_and_bound
 def optimize_bnb(prog, initialParams, target):
@@ -107,8 +135,8 @@ def optimize_hill(prog, initialParams, target):
         nextNode = None
 
         for param in L:
-            if str(param) not in seen: # don't repeat old params
-                seen.add(str(param))
+            if param not in seen: # don't repeat old params
+                seen.add(param)
                 thetime = param.process(prog)
                 for p in param.neighbors():
                     nextSet.append(p)
@@ -133,33 +161,47 @@ def optimize_hill(prog, initialParams, target):
     
     return current, currentTime
 
-def optimize_dfs(prog, initialParams, target):
-    queue = [initialParams]
+def optimize_dfs(prog, initialParams, target, epsilon = 0.1):
+    queue = [(initialParams, 0)]
     visited = set()
+    visited.add(initialParams)
     results = {}
     while len(queue) > 0:
-        current = queue.pop(0)
-        if str(current) not in visited:
-            visited.add(str(current))
+        (current, lasttime) = queue.pop()
+        thetime = current.process(prog)
 
-            thetime = current.process(prog)
-
-            #print current, thetime
-            if thetime < P:
-                for nextParams in current.successors():
-                    queue.append(nextParams)
-            else:
-                print "at end", current, thetime
-                results[current] = thetime
+        if thetime < P and abs(thetime - lasttime) >= epsilon:
+            for nextParams in current.successors():
+                if nextParams not in visited:
+                    visited.add(nextParams)
+                    queue.append((nextParams, thetime))
+        else:
+            results[current] = lasttime
     return results
 
-prog = sys.argv[1]
-P = float(sys.argv[2])
-initialParams = Argon2Params()
-params, thetime = optimize_hill(prog, initialParams, P)
-print params, thetime
+def find_min_params(results):
+    max_key = results.keys()[0]
+    max_val = results[max_key]
+    for k in results:
+        v = results[k]
+        if v > max_val:
+            max_key = k
+            max_val = v
+    return max_key
 
-initialParams = Argon2Params(1, 1, 1)
-results = optimize_dfs(prog, initialParams, P)
-print results
+
+#prog = sys.argv[1]
+#scrypt = ScryptParams(1, 1, 1)
+#print scrypt.process(prog)
+
+P_list = [2, 4, 8, 16, 32, 64, 128]
+targets = map(lambda P : int((float(1500) / (P * 1000000)) * 1000000), P_list)
+for i, P in enumerate(targets):
+    initialParams = Argon2Params(1, 1, 1)
+    results = optimize_dfs(prog, initialParams, P)
+    print "argon2", P_list[i], find_min_params(results)
+
+    #initialParams = ScryptParams(1, 1, 1)
+    #results = optimize_dfs(prog, initialParams, P)
+    #print "scrypt", P_list[i], find_min_params(results)
 
